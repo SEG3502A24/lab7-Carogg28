5ec303c89c2dcd203d6c2fb68fa66f4fac7ee8ab
Carogg28 <90726597+Carogg28@users.noreply.github.com>
2024-11-03 23:33:27 -0500

    authors works

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,24 +7,30 @@ import {BookComponent} from "./books/book/book.component";
 import {LoginComponent} from "./login/login.component";
 import {AdminComponent} from "./admin/admin.component";
 import {loggedInGuard} from "./logged-in.guard";
+import { AuthorsComponent } from './authors/authors.component';
+
 
 const booksRoutes: Routes = [
   {path: ':id', component: BookComponent}
 ];
 
+
 export const routes: Routes = [
-  {path: 'home', component: HomeComponent},
-  {path: 'about', component: AboutComponent},
-  {path: 'contact', component: ContactComponent},
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
   { path: 'login', component: LoginComponent },
   {
     path: 'admin',
     component: AdminComponent,
     canActivate: [ loggedInGuard ]
   },
-  {path: 'books', component: BooksComponent,
+  {
+    path: 'books',
+    component: BooksComponent,
     children: booksRoutes
   },
-  {path: '', redirectTo: 'home', pathMatch: 'full'},
-  {path: '**', component: HomeComponent}
+  { path: 'authors', component: AuthorsComponent },  
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', component: HomeComponent }
 ];
